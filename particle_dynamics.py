da0e75a519995d38f56665b0713846bd32c5467d
Gowtham Garimella <ggarime1@jhu.edu>
2019-09-18 17:01:29 -0400

    implemented quadratic cost

--- particle_dynamics.py
+++ particle_dynamics.py
@@ -2,9 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-x = np.array([0.,0.,1.,2.,0.])
+x = np.array([0.,0.,1.,2.])
+xg = np.array([1., 1., 1., 1.])
+R = np.diag([1., 1.])
+Q = np.diag([0., 1., 1., 1.])
+Qf = np.diag([1., 1., 0., 1.])
+t = 0.0  #initial_time
 tf = 5.0  #final_time
 dt = .02
+thist = np.array([t])
 xhist = np.array([x])
 
 def particle_dynamics(x, u):
@@ -14,15 +20,30 @@ def particle_dynamics(x, u):
     v2 = x[3] #current y velo
     a1 = u[0] #current x accel
     a2 = u[1] #current y accel
-    return np.array([v1, v2, a1, a2, 1.0]) #xdot or resultant
+    return np.array([v1, v2, a1, a2]) #xdot or resultant
 
-while x[4] < tf:
-    u = np.array([x[4]**2-x[4]+1,x[4]**2-x[4]+1])
+def quadratic_cost_for(x, u, Q, R, Qf, dt, xg):
+    a = 0
+    for i in range(0, np.size(u, 0)):
+       a += (np.dot(np.dot(np.transpose(u[i]),R), u[i]) + np.dot(np.dot(np.transpose(x[i]-xg), Q), (x[i]-xg)))*dt
+       a += np.dot(np.dot(np.transpose(x[-1]-xg), Qf), (x[-1]-xg))
+    return a
+
+u = np.array([t**2-t+1, t**2-t+1])
+uhist = np.array([u])
+t += dt
+while t < tf:
     x += particle_dynamics(x, u)*dt
+    t += dt
+    thist = np.append(thist, np.array([t]), axis=0)
     xhist = np.append(xhist, np.array([x]), axis=0)
+    u = np.array([t**2-t+1, t**2-t+1])
+    uhist = np.append(uhist, np.array([u]), axis=0)
 
+#print(np.size(xhist)) check for size of array (1008!?)
+print(quadratic_cost_for(xhist, uhist, Q, R, Qf, dt, xg))
 plt.figure()
 plt.plot(xhist[:, 0], xhist[:, 1])
 plt.figure()
-plt.plot(xhist[:, 4], (np.sqrt(xhist[:, 2]**2 + xhist[:, 3]**2)))
+plt.plot(thist[:], (np.sqrt(xhist[:, 2]**2 + xhist[:, 3]**2)))
 plt.show()
