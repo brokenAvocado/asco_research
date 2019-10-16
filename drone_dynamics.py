d53ee2f6415a57992507f5a1ccac1531ed888272
Gowtham Garimella <ggarime1@jhu.edu>
2019-10-16 17:01:05 -0400

    pt 2 of implementing tensorflow

--- drone_dynamics.py
+++ drone_dynamics.py
@@ -10,45 +10,49 @@ g = 9.81
 dt = 0.05
 tfinal = 10
 
-x0 = np.array([1., 1., 1., 1., 1., 1.]) #posx, posy, angle, velox, veloy, angVel
+x0 = tf.constant([1., 1., 1., 1., 1., 1.]) #posx, posy, angle, velox, veloy, angVel
 u0 = np.zeros((int(tfinal/dt), 2)) #thrust 1 and thrust 2
-R = tf.linalg.diag([1., 1., 1.])
-Q = tf.linalg.diag([1., 1., 1., 1., 1., 1.])
-Qf = 10*tf.linalg.diag([1., 1., 1., 1., 1., 1.])
-xg = tf.constant([2., 2., 2., 2., 2., 2.]) #goal position
+R = tf.linalg.diag([1., 1.])
+Q = tf.linalg.diag([1., 1., 1., 1.])
+Qf = 10*tf.linalg.diag([1., 1., 1., 1.])
+xg = tf.constant([2., 2., 2., 2.]) #goal position
 L1 = 10
 C1 = 100
 p1 = .016
 
 def deriv(xs, us):
     Ft = us[0] + us[1]
-    vxdot = Ft * tf.sin(xs[2]) / m
-    vydot = Ft * tf.cos(xs[2]) / m - g
+    vxdot = Ft * math.sin(float(xs[2])) / m
+    vydot = Ft * math.cos(float(xs[2])) / m - g
     wdot = 2*(us[0]-us[1])/(m * l)
-    return vxdot, vydot, wdot
+    return np.array([xs[3], xs[4], xs[5], vxdot, vydot, wdot])
 
-def hist(x0, us, dt, tf):
+def hist(x0, us, dt, tfinal):
     t = 0
     i = 0
-    xhist = tf.constant([x0])
+    x = 0
+    xhist = x0
     thist = tf.constant([t])
-    while t < tf:
-        x += deriv(xhist[-1], us[i])*dt
+    while t < tfinal:
+        x += deriv(xhist[-1], us[i]) * dt
         i += 1
         t += dt
         thist.append(t)
         xhist.append(x)
     return xhist, thist
 
-def quadratic_cost_for(x0, xg, us, Q, Qf, R, dt, tf):
-    u = tf.placeholder(tf.float32, shape = [us.shape[0], C])
-    u = tf.reshape(u, [int(u.size()/2), 2])
-    xn, tn = hist(x0, u, dt, tf)
+def quadratic_cost_for(x0, xg, us, Q, Qf, R, dt, tfinal): 
+    u = tf.reshape(us, [int(int(us.shape[0])/2), 2, C1])
+    print(u.get_shape())
+    list_cost = []
     cost = 0
-    for i in range(0, tf.shape(u)[0]):
-        cost += (tf.matmul(tf.matmul(u[i], R, transpose_a = True), u[i]) + tf.matmul(tf.matmul((xn[i]-xg), Q, transpose_a = True), (xn[i] - xg))) * dt
-    cost += tf.matmul(tf.matmul((xn[-1]-xg), Qf, transpose_a = True), (xn[-1] - xg))
-    return cost
+    for c in range(0, C1):
+        xn, tn = hist(x0, u[:][:][c], dt, tfinal)
+        for i in range(0, u.get_shape()[0]):
+            cost += (tf.matmul(tf.matmul(u[i][:][c], R, transpose_a = True), u[i][:][c]) + tf.matmul(tf.matmul((xn[i]-xg), Q, transpose_a = True), (xn[i] - xg))) * dt
+        cost += tf.matmul(tf.matmul((xn[-1]-xg), Qf, transpose_a = True), (xn[-1] - xg))
+        list_cost[i] = cost
+    return list_cost
 
 def costSort(cost):
     return sample[1]
@@ -58,7 +62,6 @@ def CEM(u0, J, E0, L, C, p): #L is the maximum iterations done and C is samples 
     sigma = E0
     sess = tf.Session()
     for l in range(0, L):
-        state_cost = []
         u_out = np.random.multivariate_normal(mu, sigma, size = C)
         J_out = sess.run(J, feed_dict = {u: u_out.T})
         print(J_out.shape)
@@ -70,11 +73,12 @@ def CEM(u0, J, E0, L, C, p): #L is the maximum iterations done and C is samples 
     return mu, sigma
 
 ur0 = np.reshape(u0, [-1])
+u = tf.placeholder(tf.float32, shape = [ur0.shape[0], C1])
 sigma0 = 10*np.diag(np.ones_like(ur0))
-cost = lambda us : quadratic_cost_for(us, R, Q, Qf, tf, dt, xg, x0)
-CEM(ur0, cost, sigma0, L1, C1, p1)
+cost = quadratic_cost_for(x0, xg, u, Q, Qf, R, dt, tfinal)
+mu, sigma = CEM(ur0, cost, sigma0, L1, C1, p1)
 
-us = tf.reshape(mu, [int(mu.size/2), 2])
+us = np.reshape(mu, [int(mu.size/2), 2])
 xhist, thist = hist(x0, us, dt, tf)
 
 plt.figure()
