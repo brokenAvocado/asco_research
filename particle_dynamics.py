6cc02a15d424576ea735c3c6c879691fe95485aa
Eric Kim <ggarime1@jhu.edu>
2019-11-27 16:47:45 -0500

    was also missing objective, and time measuring points adjusted

--- particle_dynamics.py
+++ particle_dynamics.py
@@ -2,20 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import pdb
+import time
 
 t = 0.
 dt = .02 #MUST BE EQUAL TO PARTICLE_COST dt
 tf = 5.0 #^^
 us = np.zeros((int(tf/dt), 2))
-R = np.diag([1., 1.])
-Q = 10*np.diag([1., 1., 1., 1.])
-Qf = 10*np.diag([1., 1., 1., 1.]) 
+R = 0.01 * np.diag([1., 1.])
+Q = 10 * np.diag([1., 1., 1., 1.])
+Qf = 10 * np.diag([1., 1., 1., 1.]) 
 xg = np.array([5., 5., 0., 0.]) #goal position
 x0 = np.array([2., 2.,0.,0.]) #initial position
 
 L1 = 10 #number of iterations to run
 C1 = 100 #number of samples per iterations
 p1 = 0.15 #percent of samples to keep
+
 
 def particle_dynamics(x, u):
     p1 = x[0] #current x pos
@@ -73,13 +75,16 @@ def CEM(u0, J, E0, L, C, p): #L is the maximum iterations done and C is samples 
         mu = np.mean(e_samples.T, axis = 1)
         sigma = np.cov(e_samples.T)
     return mu, sigma
-                
+
+tstart = time.perf_counter()
 us0 = np.concatenate(us,-1)
 print(us0.shape)
 sigma0 = 10*np.diag(np.ones_like(us0))
 
 cost = lambda us : particle_cost(us, R, Q, Qf, tf, dt, xg, x0)
 mu, sigma = CEM(us0, cost, sigma0, L1, C1, p1)
+tstop = time.perf_counter()
+print(tstop - tstart, "sec.")
 
 us = np.reshape(mu, (int(mu.size/2), 2))
 xhist, thist = hist(x0, us, dt, tf)
